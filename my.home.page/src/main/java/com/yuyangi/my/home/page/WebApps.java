2f67d5a5a0ec9456ee0c46fa032c62ad563fcc37
yuyangi <free_13@163.com>
2017-06-19 18:28:23 +0800

    first commit

---
+++ my.home.page/src/main/java/com/yuyangi/my/home/page/WebApps.java
@@ -0,0 +1,18 @@
+package com.yuyangi.my.home.page;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * yuyangi
+ */
+@EnableAutoConfiguration
+@SpringBootApplication
+public class WebApps 
+{
+    public static void main( String[] args )
+    {
+    	SpringApplication.run(WebApps.class, args);
+    }
+}
